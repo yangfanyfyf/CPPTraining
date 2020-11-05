a53f304f83afd9ddd09f2597a8dce7a2c0c1d464
Yang Fan <68161582+yangfanyfyf@users.noreply.github.com>
2020-11-05 17:59:11 +0800

    update11_5

---
+++ dijkstra2/dijkstra/dijkstra.h
@@ -0,0 +1,43 @@
+//
+//  dijkstra.h
+//  dijkstra
+//
+//  Created by 杨帆 on 2020/11/3.
+//
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Distances{
+    string path;
+    int value;
+    bool visit;
+    Distances(){
+        visit = false;
+        value = 0;
+        path = "";
+    }
+};
+
+class Graph{
+private:
+    int verticesNum;
+    int edgeNum;
+    //int **arc;
+    vector<vector<int>> arc;
+    
+    
+    Distances *dis;
+public:
+    Graph(int verticesNum, int edgeNum);
+    ~Graph();
+    bool check_edge_value(int start, int end, int weight);
+    void createGraph();
+    void printGraph();
+    void dijkstra(int begin);
+    void printPath(int begin);
+};
